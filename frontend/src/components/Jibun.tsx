f7e3cbb6790d42b7382350003d1fa8849b6f984e
Gergana Minova <minova.gigi@gmail.com>
2025-05-19 19:52:19 +0900

    Add routes

---
+++ frontend/src/components/Jibun.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Jibun: React.FC = () => {
+  return (
+    <div className="jibun-container">
+      <h1>Jibun</h1>
+      <p>Welcome to the Jibun section!</p>
+    </div>
+  );
+};
+
+export default Jibun; 
---
+++ frontend/src/components/Portfolio.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Portfolio: React.FC = () => {
+  return (
+    <div className="portfolio-container">
+      <h1>Portfolio</h1>
+      <p>Welcome to the Portfolio section!</p>
+    </div>
+  );
+};
+
+export default Portfolio; 
