e2f654e4338849122f6a70c4e328964dabf25698
Gergana Minova <minova.gigi@gmail.com>
2025-05-23 20:39:48 +0900

    fix lint

--- frontend/src/types/html2pdf.d.ts
+++ frontend/src/types/html2pdf.d.ts
@@ -1,4 +1,18 @@
 declare module 'html2pdf.js' {
-  const html2pdf: any;
+  interface Html2PdfOptions {
+    margin?: number;
+    filename?: string;
+    image?: { type?: string; quality?: number };
+    html2canvas?: { scale?: number };
+    jsPDF?: { unit?: string; format?: string; orientation?: string };
+  }
+
+  interface Html2PdfInstance {
+    from: (element: HTMLElement) => Html2PdfInstance;
+    set: (options: Html2PdfOptions) => Html2PdfInstance;
+    save: () => Promise<void>;
+  }
+
+  const html2pdf: (element: HTMLElement, options?: Html2PdfOptions) => Html2PdfInstance;
   export default html2pdf;
 } 
